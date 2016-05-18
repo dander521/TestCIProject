c6b57cef91bc03728a8602829945523d48e0c36a
程荣刚 <chengronggang@rongkecloud.com>
2016-05-18 16:46:32 +0800

    Add test case

--- TestCIProjectTests/TestCIProjectTests.m
+++ TestCIProjectTests/TestCIProjectTests.m
@@ -8,6 +8,27 @@
 
 #import <XCTest/XCTest.h>
 
+#define assertTrue(expr)              XCTAssertTrue((expr), @"")
+#define assertFalse(expr)             XCTAssertFalse((expr), @"")
+#define assertNil(a1)                 XCTAssertNil((a1), @"")
+#define assertNotNil(a1)              XCTAssertNotNil((a1), @"")
+#define assertEqual(a1, a2)           XCTAssertEqual((a1), (a2), @"")
+#define assertEqualObjects(a1, a2)    XCTAssertEqualObjects((a1), (a2), @"")
+#define assertNotEqual(a1, a2)        XCTAssertNotEqual((a1), (a2), @"")
+#define assertNotEqualObjects(a1, a2) XCTAssertNotEqualObjects((a1), (a2), @"")
+#define assertAccuracy(a1, a2, acc)   XCTAssertEqualWithAccuracy((a1),(a2),(acc))
+
+#define WAIT                                                                \
+do {                                                                        \
+[self expectationForNotification:@"LCUnitTest" object:nil handler:nil]; \
+[self waitForExpectationsWithTimeout:60 handler:nil];                   \
+} while(0);
+
+#define NOTIFY                                                                            \
+do {                                                                                      \
+[[NSNotificationCenter defaultCenter] postNotificationName:@"LCUnitTest" object:nil]; \
+} while(0);
+
 @interface TestCIProjectTests : XCTestCase
 
 @end
@@ -31,6 +52,18 @@
 
 - (void)testNumber {
     XCTAssertNil(nil);
+}
+
+- (void)testNumber1 {
+    XCTAssertTrue(1 == 1);
+}
+
+- (void)testNumber2 {
+    XCTAssertFalse(1 == 1);
+}
+
+- (void)testNumber3 {
+    assertEqual(2, 3);
 }
 
 - (void)testPerformanceExample {
